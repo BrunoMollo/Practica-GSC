f2ea7ccbf8e7ed5e94d47e8e07d772c3eb8752f1
BrunoMollo <dia.mollo.bruno@gmail.com>
2022-09-08 16:19:59 -0300

    Ejercicio 1.3

---
+++ Frontend/TypeScript/dist/Ej-(1.3).js
@@ -0,0 +1,9 @@
+"use strict";
+// >>Codigo inicial
+// let esPablo: boolean;
+//
+// console.log('[Ejercicio 1.3]', `${esPablo ? 'Oh, hola Pablo' : 'Quien sos vos?'}`);
+let esPablo = true;
+console.log('[Ejercicio 1.3]', `${esPablo ? 'Oh, hola Pablo' : 'Quien sos vos?'}`);
+esPablo = false;
+console.log('[Ejercicio 1.3]', `${esPablo ? 'Oh, hola Pablo' : 'Quien sos vos?'}`);
---
+++ Frontend/TypeScript/src/Ej-(1.3).ts
@@ -0,0 +1,14 @@
+// >>Codigo inicial
+// let esPablo: boolean;
+//
+// console.log('[Ejercicio 1.3]', `${esPablo ? 'Oh, hola Pablo' : 'Quien sos vos?'}`);
+
+let esPablo: boolean= true;
+
+console.log('[Ejercicio 1.3]', `${esPablo ? 'Oh, hola Pablo' : 'Quien sos vos?'}`);
+
+
+esPablo= false;
+
+console.log('[Ejercicio 1.3]', `${esPablo ? 'Oh, hola Pablo' : 'Quien sos vos?'}`);
+
