c3ced102da84cec7550cd70660e10cd154084240
BrunoMollo <dia.mollo.bruno@gmail.com>
2022-09-20 19:20:59 -0300

    Ejercicio 3.2

--- Frontend/TypeScript/src/3)Funciones/Ej-(3.1).ts
+++ Frontend/TypeScript/src/3)Funciones/Ej-(3.1).ts
@@ -6,6 +6,6 @@ function sumArray(numbers: Array<number>) {
     return numbers.reduce(add, 0);
 }
 
-const someSum = sumArray(['3', '6', '9'].map( (x)=>+x) );
+const someSum = sumArray(['3', '6', '9'].map( (x)=>+x ) );
 
 console.log('[Ejercicio 3.1]', `3 + 6 + 9 === ${someSum}`);
