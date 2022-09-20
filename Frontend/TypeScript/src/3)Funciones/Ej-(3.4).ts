a6bb5869bfec903bf15e5d3f0ec22cc63ccc0e8b
BrunoMollo <dia.mollo.bruno@gmail.com>
2022-09-20 19:33:17 -0300

    Ejercicio 3.4

---
+++ Frontend/TypeScript/src/3)Funciones/Ej-(3.4).ts
@@ -0,0 +1,7 @@
+function greet(greeting:string='hola') {
+    return greeting.toUpperCase();
+}
+    
+const defaultGreeting = greet();
+const portugueseGreeting = greet('Oi como vai!');
+console.log('[Ejercicio 3.4]', defaultGreeting, portugueseGreeting);
