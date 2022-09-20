47651ac80cf5ae3d5b56fbfe04e56cc9e5ab3f0e
BrunoMollo <dia.mollo.bruno@gmail.com>
2022-09-20 19:30:09 -0300

    Ejercicio 3.3

---
+++ Frontend/TypeScript/src/3)Funciones/Ej-(3.3).ts
@@ -0,0 +1,27 @@
+function computeScore(word:string) {
+    const letters = word.toUpperCase().split('');
+    return letters.reduce((accum, curr) => accum += getPointsFor(curr), 0);
+}
+
+function getPointsFor(letter:string) {
+    const lettersAndPoints:[string,number][] = [
+        ['AEOIULNRST', 1],
+        ['DG', 2],
+        ['BCMP', 3],
+        ['FHVWY', 4],
+        ['K', 5],
+        ['JX', 8],
+        ['QZ', 10],
+     ];
+    
+    return lettersAndPoints.reduce((computedScore, pointsTuple) => {
+        const [letters, score] = pointsTuple;
+        if (letters.split('').find((ll) => ll === letter)) {
+            return computedScore += score;
+        }
+        return computedScore;
+    }, 0);
+}
+    
+const palabra="zoologico";
+console.log('[Ejercicio 3.3]', `${palabra} vale ${computeScore(palabra)} puntos.`);
