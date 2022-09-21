c8ffd067f685a821aef32451af926189b0024d14
BrunoMollo <dia.mollo.bruno@gmail.com>
2022-09-21 13:10:37 -0300

    Ejercicio 5.2

---
+++ Frontend/TypeScript/dist/5)Clases/Ej-(5.2).js
@@ -0,0 +1,11 @@
+"use strict";
+class Person {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const jane = new Person('Juan', 31);
+console.log('[Ejercicio 5.2]', `El nombre de la nueva persona es ${jane.name}.`);
