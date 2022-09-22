55f2e0b3a53a140bfb8f1fa3e13d232cd575f9fa
BrunoMollo <dia.mollo.bruno@gmail.com>
2022-09-22 08:23:48 -0300

    add routes in app.module

--- Frontend/Angular/4-5)Servicios y Enrutameinto/ejercicio15/src/app/app.module.ts
+++ Frontend/Angular/4-5)Servicios y Enrutameinto/ejercicio15/src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EventsListComponent } from './events-list/events-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { environment } from 'src/environments/environment';
+
+const appRoutes:Routes=[
+  {path:'eventos', component: EventsListComponent}
+]
 
 @NgModule({
   declarations: [
@@ -11,6 +17,7 @@ import { EventsListComponent } from './events-list/events-list.component';
   ],
   imports: [
     BrowserModule
+    ,RouterModule.forRoot(appRoutes, { enableTracing: !environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
