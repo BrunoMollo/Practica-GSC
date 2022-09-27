ef501ee34493c323493705d16c124d0e92b85b8c
BrunoMollo <dia.mollo.bruno@gmail.com>
2022-09-27 18:30:12 -0300

    fix ejercicio23, validaciones correctas + refactoring

--- Frontend/Angular/6)Formularios/ejercicio23/src/app/create-event/create-event.component.ts
+++ Frontend/Angular/6)Formularios/ejercicio23/src/app/create-event/create-event.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {  FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { EventService } from '../event.service';
 import { Event } from '../entitys/event';
+
 
 @Component({
   selector: 'app-create-event',
@@ -11,21 +12,42 @@ import { Event } from '../entitys/event';
 })
 export class CreateEventComponent implements OnInit {
 
-
-
-  eventForm:FormGroup
+  eventForm:FormGroup;
 
   constructor(private eventService: EventService, private router: Router, private fromBuilder: FormBuilder) { 
     this.eventForm= fromBuilder.group({
-      name:['', [Validators.required, Validators.minLength(3)]],
-      date: ['', [Validators.required, Validators.minLength(6)]],
-      time: ['', [Validators.required, Validators.minLength(4)]],
+      name:['', [Validators.required]],
+      date: ['', [Validators.required]],
+      time: ['', [Validators.required]],
       location: fromBuilder.group({
-        address: ['', [Validators.required, Validators.minLength(3)]],
-        city: ['', [Validators.required, Validators.minLength(3)]],
-        country: ['', [Validators.required, Validators.minLength(3)]]
+        address: ['', [Validators.required]],
+        city: ['', [Validators.required]],
+        country: ['', [Validators.required, Validators.pattern('[A-Z][A-Z]')]]
       })
     })
+  }
+
+  hasToWarn(field:string):Boolean{
+    let input=this.eventForm.get(field);
+    if(!input) return false;
+    return input.invalid && !input.pristine;
+  }
+
+
+  inputDynamicStyles(field:string): {[klass: string]:any} {
+    if(this.hasToWarn(field)) 
+    return {'border-color':'red'}
+    else return {}
+  }
+
+
+  private failsWith(error:string, field:string):boolean{
+    return this.eventForm.get(field)?.errors?.[error]
+  }
+  getErrorMessage(field:string):string|null{
+    if(this.failsWith('pattern',field)) return "Deben ser dos letras en mayusculas"
+    if(this.failsWith('required',field)) return "Requerido"
+    else return null;
   }
 
 
