aa7e51990778fd720506c3abedb66d0aa2281588
BrunoMollo <dia.mollo.bruno@gmail.com>
2022-10-06 19:56:28 -0300

    extended ejercicio 41
    muestro el primer y ulitmo valor retornado por el observarble

--- Frontend/Angular/7)Http/ejercicio41/src/app/parent/parent.component.ts
+++ Frontend/Angular/7)Http/ejercicio41/src/app/parent/parent.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { of, lastValueFrom } from 'rxjs';
+import { of, lastValueFrom, firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-parent',
@@ -9,21 +9,24 @@ import { of, lastValueFrom } from 'rxjs';
 export class ParentComponent implements OnInit {
 
   obs;
-  result: number;
-  promise: Promise<number>;
+  firstResult: number;
+  promiseFirst: Promise<number>;
+
+  lastResult: number;
+  promiseLast: Promise<number>;
 
   constructor() { 
-    this.obs = of(35);
+    this.obs = of(35,43,57);
   }
 
   ngOnInit(): void {
-    this.promise=lastValueFrom(this.obs)
+    this.promiseFirst=firstValueFrom(this.obs)
+    this.promiseLast=lastValueFrom(this.obs)
   }
 
   listen(): void {
-    this.promise.then((val) => {
-      this.result = val;
-    });
+    this.promiseLast.then((val) => this.lastResult = val );
+    this.promiseFirst.then((val) => this.firstResult = val)
   }
 
 }
